cd145bdd8e940bbcaea1b099f6f67d74f54b05fb
robstarbuck <robstarbuck@gmail.com>
2024-03-20 16:41:58 +0000

    Bumped Storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,7 +3,6 @@ import type { Preview } from "@storybook/web-components";
 const preview: Preview = {
   parameters: {
     layout: "fullscreen",
-    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
